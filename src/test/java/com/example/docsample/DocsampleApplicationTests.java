76408d0f9c25c7404c0b6c11590a0e8360c736b1
suresh-amith <suresh-amith.it@gmail.com>
2024-02-04 04:52:58 -0600

    first commit

---
+++ src/test/java/com/example/docsample/DocsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
